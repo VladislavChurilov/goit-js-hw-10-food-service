55e273bf9fa9aa135e751fe51b9e77541846f1bf
Vladislav Churilov <88vvch88@gmail.com>
2021-02-20 17:41:20 +0200

    hw10

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -3,67 +3,40 @@ const Theme = {
     DARK: 'dark-theme',
   };
 
-
 const bodyRef = document.querySelector('body');
 const btnRef = document.querySelector('.theme-switch__toggle');
 
-
 const lightTheme = JSON.parse(localStorage.getItem('light'));
 const darkTheme = JSON.parse(localStorage.getItem('dark'));
-
 // localStorage.setItem('themes', JSON.stringify(Theme));
 // const theme = JSON.parse(localStorage.getItem("Theme"));
-
-
 
 bodyRef.classList.remove(lightTheme);
 bodyRef.classList.remove(darkTheme);
 
-if (!darkTheme) {
-  
+if (!darkTheme) {  
   localStorage.setItem('light', JSON.stringify(Theme.LIGHT));
   bodyRef.classList.add(lightTheme);
 } else{
   localStorage.setItem('dark', JSON.stringify(Theme.DARK));
   bodyRef.classList.add(darkTheme);
+  btnRef.checked = true;
 };
-
 
 btnRef.addEventListener("change", addTheme);
 
 function addTheme() {
-  localStorage.setItem('dark', JSON.stringify(Theme.DARK));
-  bodyRef.classList.toggle(darkTheme);
   bodyRef.classList.toggle(lightTheme);
-  console.log(bodyRef.className !== 'light-theme');
-  if (bodyRef.className !== 'light-theme') {
-    localStorage.removeItem('light');
-    localStorage.setItem('dark', JSON.stringify(Theme.DARK));
-    bodyRef.classList.toggle(darkTheme);
-    // btnRef.setAttribute('checked','');
-    // btnRef.checked = true;
-  } else {
-    localStorage.setItem('dark', JSON.stringify(Theme.DARK));
+  bodyRef.className === 'light-theme'? 
+  localStorage.setItem('light', JSON.stringify(Theme.LIGHT))
+  :localStorage.setItem('dark', JSON.stringify(Theme.DARK));
+  bodyRef.classList.add(darkTheme);
+  bodyRef.classList.toggle(darkTheme);
+  if (bodyRef.className === 'light-theme') {
     localStorage.removeItem('dark');
-
+    return
+  } else{
+    localStorage.setItem('dark', JSON.stringify(Theme.DARK))
+  bodyRef.classList.add(darkTheme);
   }
-  // localStorage.setItem('dark', JSON.stringify(Theme.DARK));
-
-  // bodyRef.classList.toggle(darkTheme);
-
-  // if (!darkTheme) {
-  //   console.log('ghhh');
-  //   localStorage.setItem('dark', JSON.stringify(Theme.DARK));
-  //   bodyRef.classList.toggle(darkTheme);
-  //   bodyRef.classList.toggle(lightTheme);
-  //   // btnRef.setAttribute('checked','');
-  //   // btnRef.checked = true;
-  //   localStorage.removeItem('light');
-
-  // } else{
-  //   localStorage.setItem('light', JSON.stringify(Theme.LIGHT));
-  //   bodyRef.classList.toggle(lightTheme);
-  //   localStorage.removeItem('dark');
-  // };
-
 }
