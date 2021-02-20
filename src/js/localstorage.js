77ef6bb1fabf6ba1e8a23d9c34783b2b6f70e2f9
Vladislav Churilov <88vvch88@gmail.com>
2021-02-20 03:58:43 +0200

    jhgku

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -7,164 +7,63 @@ const Theme = {
 const bodyRef = document.querySelector('body');
 const btnRef = document.querySelector('.theme-switch__toggle');
 
- localStorage.setItem('light', JSON.stringify(Theme.LIGHT));
- localStorage.setItem('dark', JSON.stringify(Theme.DARK));
+
 const lightTheme = JSON.parse(localStorage.getItem('light'));
 const darkTheme = JSON.parse(localStorage.getItem('dark'));
-// const savedTheme = localStorage.setItem('themes', JSON.stringify(Theme));
-// const theme = JSON.parse(localStorage.getItem("themes"));
-// btnRef.addEventListener("change", a);
-// addTheme()
-// function a() {
-//   if (localStorage.getItem('light') === null) {
-//     addDarkTheme()
-//   }
-//   console.log(localStorage.getItem('light'));
-// }
-// console.log(localStorage.getItem('light'));
-// addDarkTheme()
-// addTheme();
-if (true) {    
-    
-  addDarkTheme();
-} else {
-addLightTheme()
+
+// localStorage.setItem('themes', JSON.stringify(Theme));
+// const theme = JSON.parse(localStorage.getItem("Theme"));
+
+
+
+bodyRef.classList.remove(lightTheme);
+bodyRef.classList.remove(darkTheme);
+
+if (!darkTheme) {
+  
+  localStorage.setItem('light', JSON.stringify(Theme.LIGHT));
+  bodyRef.classList.add(lightTheme);
+} else{
+  localStorage.setItem('dark', JSON.stringify(Theme.DARK));
+  bodyRef.classList.add(darkTheme);
 };
+
 
 btnRef.addEventListener("change", addTheme);
 
 function addTheme() {
-  // bodyRef.classList.add(lightTheme);
-console.log(bodyRef.className);
+  localStorage.setItem('dark', JSON.stringify(Theme.DARK));
+  bodyRef.classList.toggle(darkTheme);
+  bodyRef.classList.toggle(lightTheme);
+  console.log(bodyRef.className !== 'light-theme');
+  if (bodyRef.className !== 'light-theme') {
+    localStorage.removeItem('light');
+    localStorage.setItem('dark', JSON.stringify(Theme.DARK));
+    bodyRef.classList.toggle(darkTheme);
+    // btnRef.setAttribute('checked','');
+    // btnRef.checked = true;
+  } else {
+    localStorage.setItem('dark', JSON.stringify(Theme.DARK));
+    localStorage.removeItem('dark');
 
-// addLightTheme();
-  if (true) {    
-    
-    addDarkTheme();
   }
-  addLightTheme()
+  // localStorage.setItem('dark', JSON.stringify(Theme.DARK));
+
+  // bodyRef.classList.toggle(darkTheme);
+
+  // if (!darkTheme) {
+  //   console.log('ghhh');
+  //   localStorage.setItem('dark', JSON.stringify(Theme.DARK));
+  //   bodyRef.classList.toggle(darkTheme);
+  //   bodyRef.classList.toggle(lightTheme);
+  //   // btnRef.setAttribute('checked','');
+  //   // btnRef.checked = true;
+  //   localStorage.removeItem('light');
+
+  // } else{
+  //   localStorage.setItem('light', JSON.stringify(Theme.LIGHT));
+  //   bodyRef.classList.toggle(lightTheme);
+  //   localStorage.removeItem('dark');
+  // };
+
 }
-
-function addDarkTheme() {
-
-bodyRef.classList.toggle(darkTheme);
-bodyRef.classList.remove(lightTheme);
-// localStorage.removeItem('light');
-btnRef.setAttribute('checked','');
-// btnRef.checked === true; 
-}
-
-function addLightTheme() {
-  
-  bodyRef.classList.add(lightTheme);
-  // localStorage.removeItem('dark');
-  // btnRef.checked === false;
-}
-// 
-// localStorage.removeItem('theme');
-// export default { load, save };
-
-
-
-// if (btnRef.className !== 'dark-theme') {
-//   console.log('hhh');
-//   bodyRef.classList.add(lightTheme);
-// }
-
-// btnRef.addEventListener("change", addTheme);
-// function addTheme(event) {
-//   // event.preventDefault();
-  
-//   bodyRef.classList.toggle(darkTheme);
-  
-//   if (bodyRef.className !== 'dark-theme') {
-   
-//     bodyRef.classList.toggle(lightTheme);
-//   }
-  // btnRef.setAttribute('checked','');
-  // bodyRef.classList.add(darkTheme);
-  // console.log(btnRef.checked);
-// }
-
-
-
-
-
-
-
-
-// 
-
-// btnRef.checked === false? 
-//   bodyRef.classList.add(lightTheme)
-//   : bodyRef.classList.add(darkTheme);
-// if (btnRef.checked === false) {
-//   bodyRef.classList.add(lightTheme);
-//   return
-// }
-
-
-// btnRef.checked = false;
-// a ();
-// btnRef.addEventListener("change",a);
-// function a() {
-//   if (btnRef.checked === false) {
-//     bodyRef.classList.add(lightTheme);
-//     return
-//   }
-//   bodyRef.classList.add(darkTheme);
-//   btnRef.setAttribute('checked',true);
-//   console.log(btnRef.checked);
-// }
-  
-
-// addDarkTheme();
-
-// addLightTheme()
-// function addLightTheme() {
-//   // bodyRef.classList.add(lightTheme);
-//   if (bodyRef.className === 'dark-theme'){
-//     return addDarkTheme();
-    
-//   }
-  // bodyRef.classList.add(lightTheme);
-// console.log(bodyRef.className);
-// }
-// bodyRef.classList.add(lightTheme);
-
-
-
-
-// btnRef.addEventListener("change", addDarkTheme);
-
-// function addDarkTheme() { 
-//   // 
-//   // 
-//   bodyRef.classList.toggle(lightTheme);
-//   bodyRef.classList.toggle(darkTheme);
-//   if (bodyRef.className === 'dark-theme') {
-//     btnRef.setAttribute('checked','');
-//     // console.log(bodyRef.className);
-//     btnRef.checked = true;
-//     // bodyRef.checked = true;
-//     return
-//   }
-
-
-
-
-
-  // console.log(bodyRef.className);
-  // btnRef.checked = false;
-  // btnRef.setAttribute('checked',true) 
-  // bodyRef.className !== 'dark-theme'? 
-  // btnRef.setAttribute('checked',true) 
-  // : btnRef.removeAttribute('checked'); 
-  
-// }
-
-// function labelPrevent(event) {
-//   event.preventDefault();
-// } 
-
-// console.log(btnRef.checked);
