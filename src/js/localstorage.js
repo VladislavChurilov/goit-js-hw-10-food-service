2e1fed350746e30b71ffd16c170a31af8f6310e4
Vladislav Churilov <88vvch88@gmail.com>
2021-03-23 19:57:37 +0200

    hw10

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -8,34 +8,39 @@ const btnRef = document.querySelector('.theme-switch__toggle');
 
 const currentTheme = JSON.parse(localStorage.getItem("theme"));
 
-// bodyRef.classList.remove(currentTheme);
-
-
-
 if (currentTheme !== Theme.DARK) {  
-  localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));    
+  localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));     
 } else{
   localStorage.setItem('theme', JSON.stringify(Theme.DARK));  
-  btnRef.checked = true;  
+  btnRef.checked = true;    
 };
 bodyRef.classList.add(currentTheme);
 btnRef.addEventListener("change",addTheme);
 
 function addTheme() {   
-  if (bodyRef.className !== Theme.LIGHT) {
+   bodyRef.classList.remove(JSON.parse(localStorage.getItem("theme")));   
+    
+  if (btnRef.checked === true) {    
     localStorage.setItem('theme', JSON.stringify(Theme.DARK));    
-// return
-  } 
-bodyRef.classList.toggle(currentTheme);
-// localStorage.setItem('theme', JSON.stringify(Theme.LIGHT))
-// bodyRef.classList.toggle(currentTheme);
-  if (bodyRef.className !== Theme.DARK) {
-    localStorage.setItem('theme', JSON.stringify(Theme.LIGHT))
-// return
-  } 
-  // bodyRef.classList.toggle(currentTheme);  
-  if (btnRef.checked === true) {
-    localStorage.setItem('theme', JSON.stringify(Theme.DARK));
-    // return
-  }  
+    bodyRef.classList.add(JSON.parse(localStorage.getItem("theme")));    
+    return
+  } else {    
+    localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));    
+    bodyRef.classList.add(JSON.parse(localStorage.getItem("theme")));    
+  }   
 }
+
+
+// if (bodyRef.className !== Theme.LIGHT) {    
+  //   localStorage.setItem('theme', JSON.stringify(Theme.DARK)); 
+  //   console.log('4');  
+   
+  // } 
+
+  // if (bodyRef.className !== Theme.DARK) {    
+  //   localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
+  //   console.log('5');
+    
+  // }   
+ 
+  // bodyRef.classList.toggle(currentTheme);  
